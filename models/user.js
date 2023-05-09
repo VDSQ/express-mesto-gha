319a97d513fc12c872fc041c53ff559d940ad465
VDSQ <vwdosrqk@gmail.com>
2023-05-09 19:21:17 +0500

    fix mistakes after 1st review

--- models/user.js
+++ models/user.js
@@ -1,66 +1,25 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: true,
       minlength: 2,
       maxlength: 30,
-      default: "Жак-Ив Кусто",
     },
     about: {
       type: String,
+      required: true,
       minlength: 2,
       maxlength: 30,
-      default: "Исследователь",
     },
     avatar: {
-      type: String,
-      default:
-        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: (email) => validator.isEmail(email),
-        message: "Некорректный адрес почты",
-      },
-    },
-    password: {
       type: String,
       required: true,
     },
   },
   { versionKey: false },
 );
-
-/* eslint-disable consistent-return */
-// eslint-disable-next-line func-names
-userSchema.pre("save", function (next) {
-  const user = this;
-
-  if (user.isModified("password") || user.isNew) {
-    bcrypt.genSalt(10, (saltError, salt) => {
-      if (saltError) {
-        return next(saltError);
-      }
-
-      bcrypt.hash(user.password, salt, (hashError, hash) => {
-        if (hashError) {
-          return next(hashError);
-        }
-
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    return next();
-  }
-});
 
 module.exports = mongoose.model("user", userSchema);
