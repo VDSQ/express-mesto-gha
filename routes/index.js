b4c16a92f62c0927853b09388e47378579325d51
VDSQ <vwdosrqk@gmail.com>
2023-05-22 15:21:31 +0500

    project #15, add CORS and LOG

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,23 @@ const signUpRouter = require("./signup");
 const usersRouter = require("./users");
 const cardsRouter = require("./cards");
 const auth = require("../middlewares/auth");
+const { requestLogger, errorLogger } = require("../middlewares/logger");
+const { cors } = require("../middlewares/cors");
 const NotFoundError = require("../errors/NotFoundError");
 
+router.use(requestLogger);
+router.use(cors);
+router.use("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадет.");
+  }, 0);
+});
 router.use("/signin", signInRouter);
 router.use("/signup", signUpRouter);
 router.use(auth);
 router.use("/users", usersRouter);
 router.use("/cards", cardsRouter);
+router.use(errorLogger);
 router.use((req, res, next) => {
   next(new NotFoundError("Такая страница не существует."));
 });
